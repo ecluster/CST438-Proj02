9af9e4ae3b84e04b5797cf18407f82916654bf4c
Edward Cluster <ecluster@csumb.edu>
2021-09-29 20:09:49 -0700

    Set up GET method for list of users for Admin

--- WishListApp/restAPI/views.py
+++ WishListApp/restAPI/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import UserSerializer
+from .models import User
 import json
 
-from WishListApp.restAPI.models import Item
-
 # Create your views here.
-def index(request):
-    response = json.dumps([{}])
-    return HttpResponse(response, content_type='text/json')
+class UserList(APIView):
+    def get(self, request):
+        user = User.objects.all()
+        query = self.request.GET.get('search')
+        if query is not None:
+            user = user.filter(name__contains=query) | user.filter(creator__contains=query)
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+
