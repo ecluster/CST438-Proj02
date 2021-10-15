4cd73d524f9122f0667e3b6a5fdee03ccbc730eb
Edward Cluster <ecluster@csumb.edu>
2021-10-15 15:33:15 -0700

    Fixing heroku/app 02

--- WishListApp/wsgi.py
+++ WishListApp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MainApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WishListApp.settings')
 
 application = get_wsgi_application()
