3a065f99813acce801779445657a36cbd1d10b2d
Edward Cluster <ecluster@csumb.edu>
2021-10-20 22:31:24 -0700

    User can create an item in the html page

--- restAPI/forms.py
+++ restAPI/forms.py
@@ -1,7 +1,14 @@
 from django.forms import ModelForm
 from .models import User
+from.models import Item
+from django import forms
 
 class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['userId', 'username', 'password']
+
+class CreateItemForm(ModelForm):
+    class Meta:
+        model = Item
+        fields = ['itemId', 'name', 'imageURL', 'websiteURL']
